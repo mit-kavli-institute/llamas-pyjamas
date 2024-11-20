0b651f62c2a5253f2364e8f8d49dd9da630fcd19
Sarah Hughes <sarah.hughes@ast.cam.ac.uk>
2024-11-20 09:55:48 -0500

    Updating extraction process to handle new files

--- llamas_pyjamas/Extract/extractLlamas.py
+++ llamas_pyjamas/Extract/extractLlamas.py
@@ -6,43 +6,61 @@ from   matplotlib import pyplot as plt
 from   pypeit.core.arc import detect_peaks
 from   pypeit.core import pydl
 import pickle
-
+import logging
 ####################################################################################
+
+# Enable DEBUG for your specific logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
 class ExtractLlamas:
 
-    def __init__(self,fitsfile,trace):
+    def __init__(self,trace):
 
+        self.bench = trace.bench
+        self.side = trace.side
+        self.channel = trace.channel
+        
+        if self.channel == 'red':
+            logger.warning("Red channel not implemented yet...ending extraction")
+            return
+        
         self.counts = np.zeros(shape=(trace.nfibers,trace.naxis1))
-        self.bench = trace.spectrograph
-
-        with fits.open(fitsfile) as hdu:
-            self.hdr   = hdu[0].header
-            frame = hdu[0].data.astype(np.float)
+        
+        self.hdr   = trace.hdr
+        frame = trace.data.astype(float)
         
         x  = np.arange(trace.naxis1)
         xx = np.outer(np.ones(trace.naxis2),np.arange(trace.naxis1))
 
         for ifiber in range(trace.nfibers):
 
-            print("..Extracting fiber #{}".format(ifiber))
+            logger.info("..Extracting fiber #{}".format(ifiber))
             
-            profimg  = trace.fiberProfileImg(ifiber)
+            profile  = trace.profimg[ifiber]
             
-            inprof = np.where(profimg > 0)
-            x_spec = xx[inprof]
-            f_spec = frame[inprof]
-            invvar = profimg[inprof]
+            inprof = np.where(profile > 0)
+            if inprof[0].size == 0:
+                logger.warning("No profile for fiber #{}".format(ifiber))
+                continue
+            x_spec = xx[ifiber][inprof]
+            f_spec = frame[ifiber][inprof]
+            invvar = profile[inprof]
             
             extracted = np.zeros(trace.naxis1)
             for i in range(trace.naxis1):
-                thisx        = np.where(x_spec == i)
-                extracted[i] = np.sum(f_spec[thisx]*invvar[thisx])/np.sum(invvar[thisx])
+                thisx = (x_spec == i)
+                if np.sum(thisx) > 0:
+                    extracted[i] = np.sum(f_spec[thisx]*invvar[thisx])/np.sum(invvar[thisx])
+                #handles case where there are no elements
+                else:
+                    extracted[i] = 0.0
 
             self.counts[ifiber,:] = extracted
 
 
-    def saveExtraction(self, outfile='LLAMASExtract.pkl'):
+    def saveExtraction(self):
+        outfile = f'LLAMASExtract_{self.channel}_{self.side}_{self.bench}.pkl'
         with open(outfile,'wb') as fp:
             pickle.dump(self, fp)
 
