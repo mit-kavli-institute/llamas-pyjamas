78c52d9442e6dc285fa2404be35f4d72704b2bc5
Sarah Hughes <sarah.hughes@ast.cam.ac.uk>
2025-04-17 14:12:01 -0400

    Adding index lookup table and a general file path to the bias folder for master copies

--- llamas_pyjamas/config.py
+++ llamas_pyjamas/config.py
@@ -6,5 +6,6 @@ DATA_DIR = os.path.join(BASE_DIR, "Docs/DATA")
 OUTPUT_DIR = os.path.join(BASE_DIR, "output")
 CALIB_DIR = os.path.join(BASE_DIR, "mastercalib")
 LUT_DIR = os.path.join(BASE_DIR, "LUT")
+BIAS_DIR = os.path.join(BASE_DIR, "Bias")
 # Ensure the output directory exists
 os.makedirs(OUTPUT_DIR, exist_ok=True)
