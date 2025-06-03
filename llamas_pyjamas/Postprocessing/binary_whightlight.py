c6af8228e033684187e38ce387127d405a8766d5
Sarah Hughes <sarah.hughes@ast.cam.ac.uk>
2025-06-03 12:48:29 +0100

    Adding custom bias option for binary whitelight- not yet tested fully

--- llamas_pyjamas/Postprocessing/binary_whightlight.py
+++ llamas_pyjamas/Postprocessing/binary_whightlight.py
@@ -17,11 +17,18 @@ def main():
         type=bool,
         default=False,
         help="Whether to plot the results in ds9 (default: False)")
+    
+    parser.add_argument(
+        "--bias",
+        type=str,
+        default=None,
+        help="Path to an optional bias file to use during processing"
+    )
 
     args = parser.parse_args()
 
     # Call the function QuickWhiteLightCube with the provided science file
-    QuickWhiteLightCube(args.science_file, ds9plot=args.plot)
+    QuickWhiteLightCube(args.science_file, bias=args.bias, ds9plot=args.plot)
 
 if __name__ == "__main__":
     main()
