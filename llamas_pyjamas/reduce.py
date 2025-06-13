736bc8bada02708a3e17b06289048449427d6b00
Sarah Hughes <slhughes@Sarahs-Mac-mini.local>
2025-06-13 13:20:30 -0400

    Adding in some of the functionality from the notebooks, still ongoing

--- llamas_pyjamas/reduce.py
+++ llamas_pyjamas/reduce.py
@@ -9,58 +9,106 @@ from llamas_pyjamas.config import BASE_DIR, OUTPUT_DIR, DATA_DIR, CALIB_DIR, BIA
 from llamas_pyjamas.Extract.extractLlamas import ExtractLlamas
 import llamas_pyjamas.GUI.guiExtract as ge
 from llamas_pyjamas.File.llamasIO import process_fits_by_color
+import llamas_pyjamas.Arc.arcLlamas as arc
+
+
 
 _linefile = os.path.join(LUT_DIR, '')
 
-
-def generate_traces(flat_file_dir, output_dir, channel=None):
+### This needs to be edited to handle a trace file per channel
+def generate_traces(red_flat, green_flat, blue_flat, output_dir, bias=None):
     
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    if channel is None:
-        run_ray_tracing(flat_file_dir, output_dir)
-    else:
-        assert channel in ['red', 'green', 'blue'], "Channel must be 'red', 'green', or 'blue'."
-        run_ray_tracing(flat_file_dir, output_dir, channel=channel)
+    
+    assert os.path.exists(red_flat), "Red flat file does not exist."
+    assert os.path.exists(green_flat), "Green flat file does not exist."
+    assert os.path.exists(blue_flat), "Blue flat file does not exist."
+
+    run_ray_tracing(red_flat, outpath=output_dir, channel='red', use_bias=bias)
+    run_ray_tracing(green_flat, outpath=output_dir, channel='green', use_bias=bias)
+    run_ray_tracing(blue_flat, outpath=output_dir, channel='blue', use_bias=bias)
+    print(f"Traces generated and saved to {output_dir}")
 
     return
 
 
 ###need to edit GUI extract to give custom output_dir
-def extract_flat_fields(flat_file_dir, output_dir):
+def extract_flat_field(flat_file_dir, output_dir, bias_file=None):
     
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    ge.GUI_extract(flat_file_dir, output_dir)
-
-
-    return
-
-
-def run_extraction():
+    ge.GUI_extract(flat_file_dir, output_dir=output_dir, use_bias=bias_file)
 
     return
 
 
-def relative_throughput():
+def run_extraction(science_file, bias_file, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    assert os.path.exists(science_file), "Science file does not exist."
+
+    ge.GUI_extract(science_file, output_dir=output_dir, use_bias=bias_file)
+
     return
 
 
-def correct_wavelengths(reference_file = None):
+def calc_wavelength_soln(arc_file, output_dir, bias=None):
+
+    ge.GUI_extract(arc_file, use_bias=bias, output_dir=output_dir)
+
+    arc_picklename = os.path.join(output_dir, os.path.basename(arc_file).replace('_mef.fits', '_extract.pkl'))
+
+    with open(arc_picklename, 'rb') as fp:
+        batch_data = pickle.load(fp)
     
-    reference_file = os.path.join(LUT_DIR, '')
+    arcdict = ExtractLlamas.loadExtraction(arc_picklename)
+    arcspec, metadata = arcdict['extractions'], arcdict['metadata']
+
+    arc.shiftArcX(arc_picklename)
+
+    return arcdict
+
+
+
+def relative_throughput(shift_picklename, flat_picklename):
+
+    arc.fiberRelativeThroughput(flat_picklename, shift_picklename)
+    ### need to add code in to return the name of the throughput file
+    return
+
+
+def correct_wavelengths(arcdict):
+
+    arcdict = ExtractLlamas.loadExtraction(os.path.join(LUT_DIR, 'LLAMAS_reference_arc.pkl'))
     
     return
 
 
 
 def construct_cube():
+    ###need to make a new class for this bit
     return
 
 def main(config_file):
     print("This is a placeholder for the reduce module.")
     # You can add functionality here as needed.
+    with open(config_file, 'rb') as f:
+        config = pickle.load(f)
+    print(f"Loaded configuration from {config_file}")
+    
+    try:
+        generate_traces()
+
+        extract_flat_fields()
+        run_extraction()
+        relative_throughput()
+        correct_wavelengths()
+        construct_cube()
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 
 
